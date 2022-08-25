190ef67cce0638f67b145a9f3efc02274063d86f
Moody Rahman <moody.bytes@gmail.com>
2022-08-25 02:04:01 +0000

    hard URLS for a palette

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 
+const style_data = [
+    "bg-color", 'text-color', 'accent', 'icon'
+]
+
+const onLoad = () => {
+    const url_params = new URLSearchParams(window.location.search);
+    let url_data = style_data.map((e) => {
+        return {color: url_params.get(e), name: e}
+    })
+
+    const regexp = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+    
+    if (! url_data.every((e) => e.color.match(regexp)) ) {
+        document.getElementById("wack").style.display = "inline";
+        return;
+    }
+
+    console.log(url_data)
+    url_data.forEach((e) => {
+        console.log()
+        document.documentElement.style.setProperty(`--${e.name}`, e.color);        
+    })
+
+    // document.getElementById("output").innerHTML =
+    // `:root {
+    //     --bg-color: ${bg_color};
+    //     --text-color:${text_color};
+    //     --accent: ${accent};
+    //     --icon: ${icon};
+    // }`
+
+    document.getElementById("output").innerHTML =
+    `:root {\n${url_data.reduce((prev, curr) => prev + `    --${curr.name}: ${curr.color};\n`, "") }\n}`
+}
+
 const mmm = () => {
-    console.log("clicked")
-    console.log()
     const bg_color = document.getElementById("bg-color").value;
     const text_color = document.getElementById("text-color").value;
     const accent = document.getElementById("accent").value;
@@ -17,10 +50,23 @@ const mmm = () => {
     document.documentElement.style.setProperty('--icon', icon);
 
     document.getElementById("output").innerHTML =
-        `:root {
-    --bg-color: ${bg_color};
-    --text-color:${text_color};
-    --accent: ${accent};
-    --icon: ${icon};
-}`
+    `:root {
+        --bg-color: ${bg_color};
+        --text-color:${text_color};
+        --accent: ${accent};
+        --icon: ${icon};
+    }`
+
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('bg-color', bg_color);
+    urlParams.set('text-color', text_color);
+    urlParams.set('accent', accent);
+    urlParams.set('icon', icon);
+
+    window.location.search = urlParams;
+
 }
+
+window.addEventListener('load', function(event) {
+    onLoad();
+}); 
